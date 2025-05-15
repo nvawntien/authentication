96832b9f6699e6c9a0eae44c7843988f2da2af26
Nguyen Van Tien <ngvantien1526@gmail.com>
2025-05-15 08:23:17 +0700

    connect database

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"myapp/db"
 	"myapp/handler"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	sql := &db.Sql{
+		Host:     "localhost",
+		Port:     1526,
+		UserName: "vantien1526",
+		Password: "Lpop3kiss",
+		DbName:   "golang",
+	}
+
+	sql.Connect()
+
+	defer sql.Close()
+
 	e := echo.New()
 
 	e.GET("/user/sign-in", handler.HandleSignIn)
